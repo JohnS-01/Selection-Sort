585173348f90b06271abd1aa1c67b56086af0dcb
JohnS-01 <John_Schmitz01@outlook.com>
2017-08-16 10:40:12 -0500

    Create selection_sort.js
    Implementation of selection sort.

---
+++ selection_sort.js
@@ -0,0 +1,29 @@
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+var indexOfMinimum = function(array, startIndex) {
+
+    var minValue = array[startIndex];
+    var minIndex = startIndex;
+
+    for(var i = minIndex + 1; i < array.length; i++) {
+        if(array[i] < minValue) {
+            minIndex = i;
+            minValue = array[i];
+        }
+    } 
+    return minIndex;
+}; 
+
+var selectionSort = function(array) {
+    var j = 0;
+    for (j; j < array.length; j ++){
+        var min = indexOfMinimum(array, j);
+        swap(array, j, min);
+        
+    }
+
+};
